efffbb3ae03ff009c0bae104367be9a24e51c8e3
Blessed Sibanda <n01519451g@students.nust.ac.zw>
2019-07-19 11:54:17 +0200

    configure templates and static files

--- config/settings.py
+++ config/settings.py
@@ -60,7 +60,9 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,3 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+# configure django crispy forms to use bootstrap4
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
