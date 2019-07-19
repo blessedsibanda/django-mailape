0359fdcdd517dbe99cef298ac7d9b9a4dd4f7cb4
Blessed Sibanda <n01519451g@students.nust.ac.zw>
2019-07-19 14:03:26 +0200

    fix some errors

--- mailinglist/urls.py
+++ mailinglist/urls.py
@@ -4,7 +4,7 @@ from mailinglist import views
 app_name = 'mailinglist'
 urlpatterns = [
     path('', views.MailingListListView.as_view(), name='mailinglist_list'),
-    path('new', views.CreateMailingListView, name='create_mailinglist'),
+    path('new', views.CreateMailingListView.as_view(), name='create_mailinglist'),
     path('<uuid:pk>/delete',
         views.DeleteMailingListView.as_view(),
         name='delete_mailinglist'),
--- user/views.py
+++ user/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
 
 class RegisterView(CreateView):
     form_class = UserCreationForm
     template_name = 'user/register.html'
+    success_url = '/user/login'
